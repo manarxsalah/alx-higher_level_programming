fc0e3e1c24e8f05b886ca077d732803ebfbe80e4
manarxsalah <manarsalah15@gmail.com>
2023-10-31 03:49:52 +0200

    small typo

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__wifth = value
+        self.__width = value
 
     @property
     def height(self):
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__wifth = value
+        self.__width = value
 
     @property
     def height(self):
